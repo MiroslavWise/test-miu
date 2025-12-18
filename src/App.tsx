83fa5aaafc268e0e94380a9f3823008d9036b1c6
Miroslav Wise <mipik1997@gmail.com>
2025-12-18 16:48:19 +0300

    Создание проекта

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main className="w-full flex flex-col items-center h-full">
+      <section className="w-full max-w-190 p-4 bg-[#ffffff] flex fle-col gap-4">
+        <h5 className="font-semibold text-black">Способ вывода</h5>
+        <nav className="w-full flex flex-row whitespace-nowrap">
+          
+        </nav>
+      </section>
+    </main>
+  )
+}
+
+export default App
